90b9d6692a36bc84cfdc6f049aa5761248ff64f8
Diego Mosan <diego14290764@itcg.edu.mx>
2018-10-15 16:39:52 -0500

    The text was fiexed.

--- app/Views/reviews/index.php
+++ app/Views/reviews/index.php
@@ -34,8 +34,8 @@
 							</p>
 							<p class="text-justify">
 								En un amanecer me deje viajar en los primeros sonidos y colores que se fueron bordando en muchas ideas en mi cabeza.
-								Surgieron los tintes de composiciones diversas, transportandone desde el oleaje, las palmeras,  el ir y venir de la gente, 
-								los atardeceres de Puerto Vallarta, lugar donde  conocí a Antonio Huerta<strong> " EL NIÑO "</strong>
+								Surgieron los tintes de composiciones diversas, transportandone desde el oleaje, las palmeras, el ir y venir de la gente, 
+								los atardeceres de Puerto Vallarta, lugar donde conocí a Antonio Huerta<strong> " EL NIÑO "</strong>
 								</p>
 							<div id="alejandro" class="panel-collapse collapse">
 									<p class="text-justify">
@@ -128,14 +128,14 @@
 						</p>
 						<p class="text-justify">
 							Las composiciones evidencian  estructuras musicales complejas, armónicas y melódicas, que no cualquier escucha y aún conocedor logra asimilar de primera 
-							intención.  Los acordes provocan ricas emociones por sus ricas tonalidades, secuencias, capas, sucesiones, planos, figuras melódicas suaves, fuertes, 
+							intención. Los acordes provocan ricas emociones por sus ricas tonalidades, secuencias, capas, sucesiones, planos, figuras melódicas suaves, fuertes, 
 							apasionadas, sensibles, que arrojan texturas impecables que nos invitan a escuchar una y otra vez en su aprehensión y disfrute solaz, con la mente y 
 							corazón abiertos, antes que el oído.
 						</p>
 						<div id="gustavo" class="panel-collapse collapse">
 							<p class="text-justify">
 								La instrumentación es equilibrada y destaca cada intervención instrumental y vocal, en lo particular y en su  conjunto. No hay instrumento ni participe que desentone.
-								Las participaciones individuales son dignas de encomio, voces, instrumentos, ambientaciones , concurren en la construcción de obras de arte espléndidas y sobrias. 
+								Las participaciones individuales son dignas de encomio, voces, instrumentos, ambientaciones, concurren en la construcción de obras de arte espléndidas y sobrias. 
 								La parte técnica es un dechado de detalles al máximo, en exactitud y perfección.
 								Loable la habilidad, se nota un dominio profesional de la más alta calidad .
 							</p>
@@ -147,60 +147,60 @@
 											Un estilo personal, un sello distintivo, que no recurre a imitar modelos.
 											La Producción evoca el sabor del paisaje del terruño Puerto Vallarta, jiron de la patria, las cadencias y ulular  de palmas , el aleteo de aves, el va y viene del ritmo e incesante oleaje , 
 											la brisa y su olor a sal, el canto de ballenas y aves, la coloratura azul infinita del agua y cielo, la frescura de la tierra húmeda, amaneceres y ocasos, aromas frescos de vegetación. Cada nota nos 
-											remonta a un ambiente calido, tropical y paradisíaco , donde la paz y el paisaje se sienten y se viven. Este admirable efecto sinestésico es posible por la intencionalidad musical, ambiental, sonora , 
+											remonta a un ambiente calido, tropical y paradisíaco , donde la paz y el paisaje se sienten y se viven. Este admirable efecto sinestésico es posible por la intencionalidad musical, ambiental, sonora, 
 											técnica y espiritual plasmada de las composiciones.
 										</p>
 										<p>
 											Cada pieza nos desafía a nuevas experiencias sensoriales y racionales. Hay secuencia y unidad musical, cada parte posee su personalidad y sello, el estilo compositivo es grato e identificable. 
-											Son composiciones que se disfrutan, no hay enfado , al contrario , los temas nos mantienen en suspenso por los cambios de volumen, ritmo ,melodía, las insondables profundidades armónicas, las acrobacias instrumentales , 
-											las bastantes bien logradas ambientaciones . Hay un sabor especial. 
+											Son composiciones que se disfrutan, no hay enfado, al contrario, los temas nos mantienen en suspenso por los cambios de volumen, ritmo, melodía, las insondables profundidades armónicas, las acrobacias instrumentales, 
+											las bastantes bien logradas ambientaciones. Hay un sabor especial. 
 										</p>
 									</li>
 									<br>
 									<li>
 										<p class="text-justify">
 											El desempeño musical en la composición y en la ejecución. En la composición se nota el talento  en el manejo de los ritmos , se diferencia una pieza de la otra . En las composiciones hay de todo , incluso novedades que no había 
-											escuchado en trabajos previos . A quienes gustamos  composiciones ricas en elocuencia melódica y rítmica, tenemos en este material , una rica fuente para escuchar una y otra vez, encontrando nuevos detalles en cada escucha: 
-											los títulos de las piezas, el inicio, desarrollo y final de cada pieza , el hivanado entre los temas , el conjunto de las piezas es una colección digna de atesorar en un lugar especial . Las composiciones evidencian dominio del tiempo, musicalidad , 
-											inquietudes del corazón que transitan por las venas moviéndose en los dedos y voz. Indudablemente somos seres creativos , estas obras las evidencian, nos muestran el lado humano  que prefiere la paz y creatividad como formas de comunicar , hermanar, 
-											disfrutar.  No hay monotonía sino formas y figuras que nos mantienen expectantes , con nuevos trazos, sonoridades, pausas y sensaciones. 
+											escuchado en trabajos previos. A quienes gustamos composiciones ricas en elocuencia melódica y rítmica, tenemos en este material, una rica fuente para escuchar una y otra vez, encontrando nuevos detalles en cada escucha: 
+											los títulos de las piezas, el inicio, desarrollo y final de cada pieza, el hivanado entre los temas, el conjunto de las piezas es una colección digna de atesorar en un lugar especial. Las composiciones evidencian dominio del tiempo, musicalidad, 
+											inquietudes del corazón que transitan por las venas moviéndose en los dedos y voz. Indudablemente somos seres creativos, estas obras las evidencian, nos muestran el lado humano que prefiere la paz y creatividad como formas de comunicar, hermanar, 
+											disfrutar. No hay monotonía sino formas y figuras que nos mantienen expectantes, con nuevos trazos, sonoridades, pausas y sensaciones. 
 										</p>
 										<p class="text-justify">
-											Las intervenciones de los artistas son de una plena empatía en la musicalidad , una sinergia que no sobrepone ningún elemento sobre otro , las pausas , las participaciones , los silencios , todo tiene una proporción estética del más alto nivel artísticos, 
-											estos artistas locales no tienen que pedir prestado ni copiado a músicos extranjeros , ni en virtuosismo ni el talento creativo, el conjunto produce un excelente  trabajo.  Es halagador escuchar el material tan bien elaborado.
+											Las intervenciones de los artistas son de una plena empatía en la musicalidad, una sinergia que no sobrepone ningún elemento sobre otro, las pausas, las participaciones, los silencios, todo tiene una proporción estética del más alto nivel artísticos, 
+											estos artistas locales no tienen que pedir prestado ni copiado a músicos extranjeros, ni en virtuosismo ni el talento creativo, el conjunto produce un excelente  trabajo. Es halagador escuchar el material tan bien elaborado.
 										</p>
 									</li>
 									<br>
 									<li>
 										<p class="text-justify">
-											El resultado es un evidente producto nacional , que por sus títulos , temas, contenidos , hablan de nuestra región, nuestra localidad , hay una sabor y una expresión cultural propia , digna. El cuidado de los volúmenes , la instrumentación, por ejemplo. 
-											El tiempo es un elemento que a sido manejado con esmero y sobriedad. Las composiciones tienen personalidad cada una de ellas . La destreza de Willow  es admirable , una vez más, nuestro reconocimiento a su maestría.  
-											Lo anterior, como brevísimo ejemplo , esperando exista la oportunidad de explayarnos con cada uno. Las voces , las percusiones, los instrumentos, las cuerdas , sintetizadores , todo encaja armoniosamente , hay un equilibrio sobrio, la proporción prudente y pensada, 
-											la esmerada distribución. Las letras , las palabras, la dirección, las composiciones , calificadas como admirables probablemente quede cortó ante la imposibilidad de intentar describir este trabajo monumental. México y en particular Puerto Vallarta, tiene grandes talentos y 
+											El resultado es un evidente producto nacional, que por sus títulos, temas, contenidos, hablan de nuestra región, nuestra localidad, hay una sabor y una expresión cultural propia, digna. El cuidado de los volúmenes, la instrumentación, por ejemplo. 
+											El tiempo es un elemento que a sido manejado con esmero y sobriedad. Las composiciones tienen personalidad cada una de ellas. La destreza de Willow  es admirable, una vez más, nuestro reconocimiento a su maestría.  
+											Lo anterior, como brevísimo ejemplo, esperando exista la oportunidad de explayarnos con cada uno. Las voces, las percusiones, los instrumentos, las cuerdas, sintetizadores, todo encaja armoniosamente, hay un equilibrio sobrio, la proporción prudente y pensada, 
+											la esmerada distribución. Las letras, las palabras, la dirección, las composiciones, calificadas como admirables probablemente quede cortó ante la imposibilidad de intentar describir este trabajo monumental. México y en particular Puerto Vallarta, tiene grandes talentos y 
 											el equipo conformado por Antonio Huerta es ejemplo del más alto nivel de calidad en el medio artístico. 
 										</p>
 									</li>
 									<br>
 									<li>
 										<p class="text-justify">
-											El trabajo profesional es impecable , no hay porque acudir a estudios extranjeros para lograr calidad técnica.  Significa un arduo esfuerzo de 57 meses , 1986 días enteros soñando transpirando , respirando , imaginando sonidos y aportando creatividad a magnas proporciones. 
-											Un adagio dice que los ensallos determinarán si habrá función y que la función evidenciara cuantos ensallos hubo.  El producto final muestra el arduo ensimismamiento para ofrecer el mejor esfuerzo  , la concentración  y dedicación resaltan en el producto, que muy bien vale toda la pena.
-											El trabajo técnico es impecable destaca la presicion en todo , entradas, salidas, volúmenes, equilibrio, mezcla de las partes , no hay si quiera una mínima queja, al contrario, la exactitud y sonoridad llena de exigencia de profesionalismo y no hay que pedir en absoluto a espacios foráneos, 
-											pues la presicion  y calidad logradas son inmejorables . Difícil intentar expresarlo con palabras , porque nos gana la emoción, el placer es pleno y los pensamientos sobrepasan cualquier explicación, celebremos ser de los primeros en escuchar la proeza de tanto tiempo bien invertido. 
-											Congratulémonos con la firmeza de su amistad y camaradería  , y porque no , de su hermandad incondicional , al compartirnos este parto glorioso que da a luz a nuevos hijos, estas composiciones -simiente engendrada-ahora depositada en nuestras manos , es testimonio del andar perseverante de un 
-											autor y un grupo de profesionales que apuestan al arte, que con la música es posible transformar al mundo, que si todos estuviésemos algo similar , escribiendo, esculpiendo, danzando, pintando , actuando , expresando , componiendo  , cantando , tocando , este planeta sería mejor. 
+											El trabajo profesional es impecable, no hay porque acudir a estudios extranjeros para lograr calidad técnica. Significa un arduo esfuerzo de 57 meses, 1986 días enteros soñando transpirando, respirando, imaginando sonidos y aportando creatividad a magnas proporciones. 
+											Un adagio dice que los ensallos determinarán si habrá función y que la función evidenciara cuantos ensallos hubo.  El producto final muestra el arduo ensimismamiento para ofrecer el mejor esfuerzo, la concentración y dedicación resaltan en el producto, que muy bien vale toda la pena.
+											El trabajo técnico es impecable destaca la presicion en todo, entradas, salidas, volúmenes, equilibrio, mezcla de las partes, no hay si quiera una mínima queja, al contrario, la exactitud y sonoridad llena de exigencia de profesionalismo y no hay que pedir en absoluto a espacios foráneos, 
+											pues la presicion y calidad logradas son inmejorables. Difícil intentar expresarlo con palabras, porque nos gana la emoción, el placer es pleno y los pensamientos sobrepasan cualquier explicación, celebremos ser de los primeros en escuchar la proeza de tanto tiempo bien invertido. 
+											Congratulémonos con la firmeza de su amistad y camaradería, y porque no, de su hermandad incondicional, al compartirnos este parto glorioso que da a luz a nuevos hijos, estas composiciones -simiente engendrada-ahora depositada en nuestras manos, es testimonio del andar perseverante de un 
+											autor y un grupo de profesionales que apuestan al arte, que con la música es posible transformar al mundo, que si todos estuviésemos algo similar, escribiendo, esculpiendo, danzando, pintando , actuando, expresando, componiendo, cantando, tocando, este planeta sería mejor. 
 										</p>
 										<p class="text-justify">
-											El trabajo es ejemplo para nosotros , para las oficinas de gobiernos para apoyarlo y difundirlo , para promotores y gestores para hacercar este esfuerzo a públicos y formar nuevos espectadores  y partícipes, para los medios en difundirlo , para los expertos en recomendar, para los amantes de la música 
-											para incorporar al acervo sonoro de nuestra  región como patrimonio cultural vivo.
+											El trabajo es ejemplo para nosotros, para las oficinas de gobiernos para apoyarlo y difundirlo, para promotores y gestores para hacercar este esfuerzo a públicos y formar nuevos espectadores  y partícipes, para los medios en difundirlo, para los expertos en recomendar, para los amantes de la música 
+											para incorporar al acervo sonoro de nuestra región como patrimonio cultural vivo.
 										</p>
 										<p class="text-justify">
-											Reciban nuestros parabienes por el fruto de su esfuerzo amistad y creatividad que la crítica tenga la oportunidad de reconocer la excelencia en esta discográfica, su calidad es garantía plena . La creatividad , talento y profesionalismo , valores destacados, depurados , estilizados , refinados están en cada pieza.  
+											Reciban nuestros parabienes por el fruto de su esfuerzo amistad y creatividad que la crítica tenga la oportunidad de reconocer la excelencia en esta discográfica, su calidad es garantía plena. La creatividad, talento y profesionalismo, valores destacados, depurados, estilizados, refinados están en cada pieza.  
 											Estas obras merecen un lugar especial a valorar en nuestro tiempo de disfrute por lo bello y especial. 
 										</p>
 										<p class="text-justify">
 											<strong>
-												Un trabajo de esta magnitud debe estar orgullosamente en repositorios digitales, en secciones culturales impresas, en la radio cultural local y extranjera, en boca de quienes amamos la música y abrazamos a sus artífices y ejecutantes . Gracias por el sudor y talento, por compartirnos estos hijos presumibles con orgullo y dignidad. 
+												Un trabajo de esta magnitud debe estar orgullosamente en repositorios digitales, en secciones culturales impresas, en la radio cultural local y extranjera, en boca de quienes amamos la música y abrazamos a sus artífices y ejecutantes. Gracias por el sudor y talento, por compartirnos estos hijos presumibles con orgullo y dignidad. 
 												Enhorabuena, Antonio Huerta, a cada integrante que puso amoroso su grano de arena para construir está belleza que estamos seguros pasa a las páginas de la historia. Felicidad y felicidades, nos regosijamos conjuntamente para disfrutar este magnífico trabajo de artistas mexicanos éxito, prosperidad y larga vida , ¡salud y saludos!.
 											</strong>
 										</p>
@@ -235,7 +235,7 @@
 						<p class="text-justify">
 							<strong>“El Niño” </strong>como se le conoce de cariño, no es ningún novato, no es una persona que toma las cosas a la ligera, durante los últimos seis años estuvo trabajando a sol y sombra 
 							e incluso remando contra corriente y aferrado a sacar a flote sus Idea musicales. La música es como la matemática, una ocasión me confeso El Niño, 
-							pero lo bonito de la música es que se siente, se escucha.  Aquellos años, cuando empezaba con las inquietudes en materia de composición, formo parte del taller de Rafael García Cueva, 
+							pero lo bonito de la música es que se siente, se escucha. Aquellos años, cuando empezaba con las inquietudes en materia de composición, formo parte del taller de Rafael García Cueva, 
 							un eslabón perdido entre los pentagramas, pero al que con cariño yo considero el abuelo de la experimentación en Jalisco. El mismo Cueva, lo incentivo años más tarde a que recorriera conceptos 
 							como la Acusmática y el legado de Julian Carrillo (Sonido 13), es por eso que se le dedica con mucho cariño un tema al Maestro García Cueva. 
 						</p>
